7b921c1744ee3677bf3ab92d155c0b9867931a54
Jean Flach <jean-marcel.flach@netways.de>
2015-11-06 16:50:20 +0100

    Implement REST keys

--- alertops_notification.pl
+++ alertops_notification.pl
@@ -6,6 +6,7 @@ use HTTP::Request::Common qw(POST);
 use LWP::UserAgent;
 use Data::Dumper;
 use Getopt::Long;
+use JSON;
 
 my $api_url    = 'http://notify.alertops.com';
 my $api_path   = '/RESTAPI.svc/POSTAlertv2/generic/';
@@ -33,17 +34,55 @@ my $api_key = $data{'CONTACTPAGER'}; # API KEY
 my $state = $data{'HOSTSTATE'};
 $state = $data{'SERVICESTATE'} if ($object eq 'service');
 
+my %json_hash = ('subject', $data{'SUBJECT'});
+
+my $incident_key = 'NONE';
+if ($data{'INCIDENT'}) {
+	$incident = 'incident';
+	$json_hash{'incident'} = $data{'INCIDENT'};
+}
+
+my $severity_key = 'NONE';
+if ($data{'SEVERITY'}) {
+	$severity = 'severity';
+	$json_hash{'severity'} = $data{'SEVERITY'};
+}
+
+my $url_key = 'NONE';
+if ($data{'URL'}) {
+	$url_key = 'url';
+	$json_hash{'url'} = $data{'URL'};
+}
+
+my $short_text = 'NONE';
+if ($data{'SHORTMSG'}) {
+	$short_text = 'short_text';
+	$json_hash{'short_text'} = $data{'SHORTMSG'};
+}
+
+my $long_text = 'NONE';
+if ($data{'LONGMSG'}) {
+	$long_text = 'long_text';
+	$json_hash{'long_text'} = $data{'LONGMSG'};
+}
+
+my $assignee_key = 'NONE';
+if ($data{'ASSIGNEE'}) {
+	$url_key = 'assignee';
+	$json_hash{'assignee'} = $data{'ASSIGNEE'};
+}
+
 my @params = (
   'Icinga',           # Soure
   'Icinga',           # Source Name
-  'subjectKey',       # Subject key
-  'NONE',             # Incident id key
+  'subject',          # Subject key
+  $incident_key,      # Incident id key
   $state,             # Status key
-  'NONE',             # Severity key
-  'NONE',             # Url key
-  'NONE',             # Short text key
-  'NONE',             # Long text key
-  'NONE'              # Assignee key
+  $severity_key,      # Severity key
+  $url_key,           # Url key
+  $short_text,        # Short text key
+  $long_text,         # Long text key
+  $assignee_key       # Assignee key
 );
 
 my $fields;
@@ -52,7 +91,10 @@ for my $param (@params) {
   $fields .= "/$param";
 }
 
+my $json = encode_json \%json_hash;
 my $req = POST($api_url . $api_path . $api_key . $fields);
+$req->header( 'Content-Type' => 'application/json' );
+$req->content( $json );
 
 my $ua = LWP::UserAgent->new;
 my $resp = $ua->request($req);
